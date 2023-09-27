6a313dc14d346943242f222793390f48cd21d4c0
Csaba Maczo <maczocsaba@gmail.com>
2023-09-27 23:31:20 +0100

    Tunnel pieces now disappear at a specific point

--- Assets/Scripts/TunnelManager.cs
+++ Assets/Scripts/TunnelManager.cs
@@ -1,43 +1,60 @@
 using System.Collections;
-using System.Collections.Generic;
-using System.Net;
 using UnityEngine;
+
 
 public class TunnelManager : MonoBehaviour
 {
     [SerializeField] Transform _startingPoint;
-    [SerializeField] Transform _endingPoint;
-    [SerializeField] TunnelPiece[] _tunnelPieces;
-    private void Awake()
+    [SerializeField] GameObject _endWall;
+    [SerializeField] GameObject _pullingPoint;
+    [SerializeField] GameObject[] _tunnelPieces;
+    [SerializeField] float _speed = 2.0f;
+    Rigidbody _rb;
+    Vector3 _summonPoint;
+    //List<GameObject> _tunnel = new List<GameObject>();
+    Vector3 _direction = new Vector3(1,0,0);
+
+    void Awake()
     {
+        _rb = _pullingPoint.GetComponent<Rigidbody>();
+        _summonPoint = _startingPoint.position + new Vector3 (5,0,0);
     }
     void OnEnable()
     {
         StartCoroutine(StartMovement());
-        
+    }
+    void Update()
+    {
+        _rb.velocity = _direction * _speed;
+
+    }
+    void OnDrawGizmos() 
+    {
+        Vector3 start = _startingPoint.position;
+        Vector3 end = _endWall.transform.position;
+        Vector3 summon = _startingPoint.position + new Vector3(5, 0, 0);
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(start, end);
+        Gizmos.DrawSphere(summon, 0.5f);
     }
     IEnumerator StartMovement() 
     {
-        GrabBag<TunnelPiece> grabBag = new GrabBag<TunnelPiece>(_tunnelPieces);
+        GrabBag<GameObject> grabBag = new GrabBag<GameObject>(_tunnelPieces);
 
         while (true)
         {
-            var piece = grabBag.Grab();
-            if (piece == null)
+
+            var pieceFromGrabBag = grabBag.Grab();
+            if (pieceFromGrabBag == null)
             {
                 Debug.LogError("Unable to choose a random destination for the Bee. Stopping Movement");
                 yield break;
             }
-            Instantiate(piece);
-            piece.Launch(_startingPoint.position, _endingPoint.position);
+            var piece = Instantiate(pieceFromGrabBag);
+            piece.transform.parent = _pullingPoint.transform;
 
-            
             yield return new WaitForSeconds(5.0f);
         }
     }
-    void Fire()
-    {
-        TunnelPiece tunnelPiece = PoolManager.Instance.GetTunnelPiece();
-        tunnelPiece.Launch( _startingPoint.position, _endingPoint.position);
-    }
+
 }
--- Assets/Scripts/TunnelPiece.cs
+++ Assets/Scripts/TunnelPiece.cs
@@ -1,43 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Pool;
 
 public class TunnelPiece : MonoBehaviour
 {
-    [SerializeField] float _speed = 2.0f;
-    Vector3 _destroyPoint ;
     ObjectPool<TunnelPiece> _pool;
-    Vector3 _direction;
-    Rigidbody _rb;
-
-    void Awake()
+    void OnTriggerEnter(Collider collision)
     {
-        _rb = GetComponent<Rigidbody>();
-        _direction = new Vector3(1, 0, 0);
-        _destroyPoint = new Vector3(100,100,100);
-    }
-    void Update()
-    {
-        _rb.velocity =  _direction * _speed;
-        if (Vector3.Distance(_rb.transform.position, _destroyPoint) < 1.0f)
-            Destruct();
-        Debug.Log(Vector3.Distance(_rb.transform.position, _destroyPoint));
-    }
-    public void Destruct()
-    {
-        // _pool.Release(this);
-        Debug.Log("Destroyed");
-        
-             Destroy(gameObject);
+        if (collision.gameObject.CompareTag("EndWall"))
+            Destroy(gameObject);
     }
 
-    public void Launch( Vector3 position, Vector3 endPosition )
-    {
-        transform.position = position;
-        _destroyPoint = endPosition;
-        Debug.Log(endPosition);
-    }
     public void SetPool(ObjectPool<TunnelPiece> pool)
     {
         _pool = pool;
