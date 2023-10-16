f9e7f826a275b1ef4ee3241db0c165d15793abf0
TripleJumpSS <shueyshear1@gmail.com>
2023-10-17 00:21:03 +0100

    Shark Can Eat You
    Game Over is possible.
    Simply touch the shark's mouth.
    Game Over screen has the final score tally.
    Also a Retry button that restarts the game.

--- Assets/GameSpeed.cs
+++ Assets/GameSpeed.cs
@@ -1,10 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameSpeed : MonoBehaviour
 {
     public GameObject PauseScreen;
+    public GameObject GameOverScreen;
+    public TMP_Text Points; public TMP_Text DistanceUIText; 
     void Start()
     {
         PauseScreen.SetActive(false);
@@ -21,4 +27,10 @@ public class GameSpeed : MonoBehaviour
 
     public void UnPause()
     {Time.timeScale = 1f; PauseScreen.SetActive(false);}
+    public void GameOver()
+    {Time.timeScale = 0f; GameOverScreen.SetActive(true); Points.text = DistanceUIText.text;}
+    public void Retry()
+    {
+        {Time.timeScale = 1f; SceneManager.LoadScene("Tunnel Scene");}
+    }
 }
