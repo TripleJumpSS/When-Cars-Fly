f8d6dd3f92da8a070a4455c8bce8814cfd074a71
TripleJumpSS <shueyshear1@gmail.com>
2023-10-20 14:50:54 +0100

    Fixed UI bite animation

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,10 +6,12 @@ public class Menu : MonoBehaviour
 {
     Animator _animation;
     float _transitionTime = 1.5f;
+    public bool PlayTheEntryBite;
 
     private void Awake()
     {
         _animation = GetComponent<Animator>();
+        if(PlayTheEntryBite)_animation.SetTrigger("AnimOnAwake");
     }
     public void PlayButton() 
     {
