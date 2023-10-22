eb94df381e64d93211157c5ecf6b9d02ffafb729
TripleJumpSS <shueyshear1@gmail.com>
2023-10-22 21:10:29 +0100

    Player Movement is Clamped!
    The player's upper, lower and lateral movement is now capped in the Mouse Position script.
    This prevents the player from moving the character too high, low or sideways.

--- Assets/Scripts/MousePosition.cs
+++ Assets/Scripts/MousePosition.cs
@@ -15,15 +15,20 @@ public Camera screenCamera;
 //The desired distance from the camera to the object
 public float zDistance;
 
+public float xMin; public float xMax;
+public float yMin; public float yMax;
+
 
 
 void Update () 
 {
     var mousePos = Input.mousePosition;
+    var mousePosX = Mathf.Clamp(mousePos.x, xMin, xMax);
+    var mousePosY = Mathf.Clamp(mousePos.y, yMin, yMax);
    
     // Set the position of the transform to a position defined by the mouse
     // which is zDistance units away from the screenCamera
-    this.transform.position = screenCamera.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, zDistance));
+    this.transform.position = screenCamera.ScreenToWorldPoint(new Vector3(mousePosX, mousePosY, zDistance));
 
 }
 
